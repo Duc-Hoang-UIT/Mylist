a452e5a3cf8498185473291c43993fe5b5d16528
PRID021 <Jad410.00@gmail.com>
2020-09-07 19:50:41 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+namespace Mylist
+{
+
+    class Program
+    {   
+         static void list_print(string path)
+         {
+           
+            DirectoryInfo d = new DirectoryInfo(path);
+            var directories = Directory.GetDirectories(path);
+            FileInfo[] files = d.GetFiles();
+            foreach(FileInfo f in files)
+            {
+                WriteLine(f.Name);
+            }
+            foreach(var i in directories)
+            {
+                WriteLine(i);
+            }
+         }
+        static void creat_folder(string path,string folderName)
+        {
+            string pathString = System.IO.Path.Combine(path,folderName);
+            System.IO.Directory.CreateDirectory(pathString);
+            // string filePath = System.IO.Path.Combine(pathString, "hello.txt");
+            // System.IO.File.Create(filePath);
+        }
+        static void delete(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path);
+            }
+        }
+        static void Main(string[] args)
+        {
+            creat_folder(args[0],args[1]);
+        }
+    }
+}
